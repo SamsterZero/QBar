dd9f72f668f49a9db1c08b132e1b129a776a9b7c
Vinay <vne.vvm@gmail.com>
2025-09-25 23:18:13 +0530

    Made change to default value to GenerateCodeRequest

--- src/main/java/in/vvm/qbar/dto/GenerateCodeRequest.java
+++ src/main/java/in/vvm/qbar/dto/GenerateCodeRequest.java
@@ -5,7 +5,7 @@ import lombok.Data;
 @Data
 public class GenerateCodeRequest {
     private String text;
-    private String type = "QR"; // default
+    private String type = "QR_CODE"; // default
     private int width = 300;
     private int height = 300;
 }
